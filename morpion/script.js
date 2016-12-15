97d752444d59c4cd8d15a928d1760edd484d6216
Ayoub <ayoub553@hotmail.fr>
2016-12-15 10:40:23 +0100

    morpion fini

--- morpion/script.js
+++ morpion/script.js
@@ -40,13 +40,40 @@ function recommencer() {
 
 function vainqueur() {
   for (var i = 0; i < $tableau.length; i++){
-  if ($tableau[0].classList.contains("player1") && $tableau[1].classList.contains("player1") && $tableau[2].classList.contains("player1")){
-    console.log("hey");
-    alert("tu as gagné !!");
-    recommencer();
-  }
-}
 
+  if ($tableau[0].classList.contains("player1") && $tableau[1].classList.contains("player1") && $tableau[2].classList.contains("player1") || // verticale pour joueur1
+      $tableau[3].classList.contains("player1") && $tableau[4].classList.contains("player1") && $tableau[5].classList.contains("player1") ||  // verifie pour choisir gagnant
+      $tableau[6].classList.contains("player1") && $tableau[7].classList.contains("player1") && $tableau[8].classList.contains("player1") ||
+      $tableau[0].classList.contains("player1") && $tableau[3].classList.contains("player1") && $tableau[6].classList.contains("player1") ||  // horizotale pour j1
+      $tableau[1].classList.contains("player1") && $tableau[4].classList.contains("player1") && $tableau[7].classList.contains("player1") ||
+      $tableau[2].classList.contains("player1") && $tableau[5].classList.contains("player1") && $tableau[8].classList.contains("player1") ||
+      $tableau[0].classList.contains("player1") && $tableau[4].classList.contains("player1") && $tableau[8].classList.contains("player1") || // Verifie les diagonale !!
+      $tableau[2].classList.contains("player1") && $tableau[4].classList.contains("player1") && $tableau[6].classList.contains("player1")){  // pour le joueur 1
+
+      console.log("hey");
+      alert("Player 1 Win !!");
+      recommencer();
+  }
+  // affiche que le joueur 2 a gagné !
+
+  else if ($tableau[0].classList.contains("player2") && $tableau[1].classList.contains("player2") && $tableau[2].classList.contains("player2") || // horizotale pour joueur2
+           $tableau[3].classList.contains("player2") && $tableau[4].classList.contains("player2") && $tableau[5].classList.contains("player2") ||
+           $tableau[6].classList.contains("player2") && $tableau[7].classList.contains("player2") && $tableau[8].classList.contains("player2") ||
+           $tableau[0].classList.contains("player2") && $tableau[3].classList.contains("player2") && $tableau[6].classList.contains("player2") || // verifie la verticale
+           $tableau[1].classList.contains("player2") && $tableau[4].classList.contains("player2") && $tableau[7].classList.contains("player2") ||
+           $tableau[2].classList.contains("player2") && $tableau[5].classList.contains("player2") && $tableau[8].classList.contains("player2") ||
+           $tableau[0].classList.contains("player2") && $tableau[4].classList.contains("player2") && $tableau[8].classList.contains("player2") || // Verifie les diagonale !!
+           $tableau[2].classList.contains("player2") && $tableau[4].classList.contains("player2") && $tableau[6].classList.contains("player2")) { // pour le joueur2
+
+             alert("Player2 Win !!");
+             recommencer();
+           }
+
+           else if (click == 9) {
+             alert("Match nul !!");
+             recommencer();
+           }
+        }
 }
 
 
